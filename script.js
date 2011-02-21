a9ec31d5408b68f9fe2aae561b2a0c90286fa977
Jacob Thornton <jacob@twitter.com>
2011-02-20 22:34:22 -0800

    readds support for idOrDoneIsID

--- script.js
+++ script.js
@@ -32,9 +32,10 @@
     doc.readyState = "loading";
   }
   win.$script = function(paths, idOrDone, optDone) {
-    var done = typeof idOrDone == 'function' ? idOrDone : (optDone || noop),
+    var idOrDoneIsId = typeof idOrDone == s,
+        done = (idOrDoneIsId ? optDone : idOrDone) || noop,
         paths = typeof paths == s ? [paths] : paths,
-        id = typeof idOrDone == s ? idOrDone : paths.join(''),
+        id = idOrDoneIsId ? idOrDone : paths.join(''),
         queue = paths.length,
         callback = function() {
           if (!--queue) {
