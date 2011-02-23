351544ffb369eb2cb949940860d971c2aa5bff3e
Yesudeep Mangalapilly <gora.khargosh@gmail.com>
2011-02-23 14:26:38 +0530

    Removes the licence header from src/script.js
    Signed-off-by: Yesudeep Mangalapilly <gora.khargosh@gmail.com>

--- src/script.js
+++ src/script.js
@@ -1,9 +1,3 @@
-/*!
- * $script.js v1.1
- * http://dustindiaz.com/scriptjs
- * Copyright: Dustin Diaz 2011
- * License: Creative Commons Attribution: http://creativecommons.org/licenses/by/3.0/
- */
 (function(win, doc) {
   var script = doc.getElementsByTagName("script")[0],
       list = {}, ids = {}, delay = {}, noop = function(){}, re = /loaded|complete/,
