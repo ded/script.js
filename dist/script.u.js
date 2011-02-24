c9c89a41721f5157048b191a802c175f71e90e8a
= <polvero@gmail.com>
2011-02-23 18:36:48 -0800

    change header to point to project on github

--- dist/script.u.js
+++ dist/script.u.js
@@ -1,6 +1,6 @@
 /*!
  * $script.js v1.1
- * http://dustindiaz.com/scriptjs
+ * https://github.com/polvero/script.js
  * Copyright: @ded & @fat
  * Dustin Diaz, Jacob Thornton 2011
  * License: Creative Commons Attribution: http://creativecommons.org/licenses/by/3.0/
